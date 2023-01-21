c2b7fb0bfda5026f3df8ed52ec5a3c370c074c60
Behzad-Amanpour <116773990+Behzad-Amanpour@users.noreply.github.com>
2023-01-21 10:29:52 +0330

    Update code.py

--- code.py
+++ code.py
@@ -9,20 +9,21 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
 
 model = DecisionTreeClassifier()
-scores = cross_val_score(model, X, y, cv=5, scoring='accuracy') # scoring='recall' 
+scores = cross_val_score(model, X, y, cv=5, scoring='accuracy')   # scoring could be 'recall', 'precision', 'f1', ... 
 print("cross-val accuracy:", np.mean(scores))
 
 # Regularization (Pruning) ---- Behzad Amanpour ------------------------
 model = DecisionTreeClassifier()
 model.fit(X, y)
-model.get_depth()
-model.get_n_leaves()
-
+print( model.get_depth() )  
+print( model.get_n_leaves() )
 model = DecisionTreeClassifier(max_depth=3, min_samples_leaf=2, max_leaf_nodes=10)
+                                 # "max_depth" is based on "model.get_depth"
+                                 # "max_leaf_nodes" is based on "model.get_n_leaves()"
 scores = cross_val_score(model, X, y, cv=5, scoring='accuracy')
 print("cross-val accuracy:", np.mean(scores))
 
-# Grid Search ----------------  Behzad Amanpour ------------------------- 
+# Optimization (Grid Search) ----------------  Behzad Amanpour ------------------------- 
 from sklearn.model_selection import GridSearchCV
 
 param_grid = {
